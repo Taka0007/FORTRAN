0d850ec23eb2456e8e596e1391eb04b13bdf716d
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-15 13:03:58 +0900

    Update 02.f95

--- computer mathematics/task/02.f95
+++ computer mathematics/task/02.f95
@@ -1,55 +1,56 @@
-PROGRAM Ans
-	
+Program Ans
+Implicit None
+
 INTEGER I,J
-REAL A,B,pi,S_1,S_2,ac_sin1,ac_sin2,rad1,rad2
-	
+REAL A,B,pi,S_1,S_2,ac_sin1,ac_sin2,rad1,rad2,error1,error2
+
 pi = 3.141592
 A   = 1.0
 B   = 1.0
 S_1 = 0.0
 S_2 = 0.0
-	
+
 !pi/4の時
 rad1 = pi/4
 ac_sin1 = SIN(rad1)
-	
-DO I=1,10
-	! (1/階乗)の計算
-	A = A/I
-	
-	IF (I%2==1) THEN
-		S_1 = S_1 + (A*rad1)
-	END IF
-	
-	IF (I%2==0) THEN
-		S_1 = S_1 - (A*rad1)
-	END IF
-	
-	IF (I==3 OR I==5 OR I==7) THEN
-		error1 = ABS(ac_sin1-S_1)/ac_sin1
-		WRITE(*,*)I,'項目までの誤差は',error1
-	END IF
+
+DO I=1,20
+ ! (1/階乗)の計算
+ A = A/I
+
+ IF (MOD(I,4)==3) THEN
+  S_1 = S_1 - (A)*(rad1**I)
+ END IF
+
+ IF (MOD(I,4)==1) THEN
+  S_1 = S_1 + (A)*(rad1**I)
+ END IF
+
+ IF (I==6 .OR. I==10 .OR. I==14) THEN
+  error1 = ABS(ac_sin1-S_1)/ac_sin1
+  WRITE(*,*)I/2,'項目までの誤差は',error1
+ END IF
 END DO
-	
+
 !pi/2の時
 rad2 = pi/2
 ac_sin2 = SIN(rad2)
 	
-DO J=1,10
-	! (1/階乗)の計算
-	B = B/J
-	
-	IF (J%2==1) THEN
-		S_2 = S_2 + (B*rad2)
-	END IF
-		
-	IF (J%2==0) THEN
-		S_2 = S_2 - (B*rad2)
-	END IF
-			
-	IF (J==3 OR J==5 OR J==7) THEN
-		error2 = ABS(ac_sin2-S_2)/ac_sin2
-		WRITE(*,*)J,'項目までの誤差は',error2
-	END IF
+DO J=1,20
+ ! (1/階乗)の計算
+ B = B/J
+
+ IF (MOD(J,4)==3) THEN
+  S_2 = S_2 - (B)*(rad2**J)
+ END IF
+
+ IF (MOD(J,4)==1) THEN
+  S_2 = S_2 + (B)*(rad2**J)
+ END IF
+
+ IF (J==6 .OR. J==10 .OR. J==14) THEN
+  error2 = ABS(ac_sin2-S_2)/ac_sin2
+  WRITE(*,*)J/2,'項目までの誤差は',error2
+ END IF
 END DO
-END PROGRAM Ans
+END Program Ans
