5f2b1630a486cee4ab4509a45c60ace7f511869e
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-16 14:19:16 +0900

    Create 04-2.f95

---
+++ computer mathematics/task/04-2.f95
@@ -0,0 +1,34 @@
+!ニュートン法で方程式の近似解を求める
+Program task4
+Implicit None
+
+REAL x
+INTEGER I
+DOUBLE PRECISION error
+
+READ *, error
+x = 4.0 !初期値
+
+DO WHILE( ABS(f(x))<error )THEN
+ x = x - f(x)/df(x)
+END DO
+
+STOP
+
+CONTAINS
+
+REAL FUNCTION f(x)
+ Implicit None
+ REAL x
+ f = 2*(x**3)-3*(x**2)-12*x+5
+ RETURN
+END FUNCTION f
+
+REAL FUNCTION df(x)
+ Implicit None
+ REAL x
+ df = 6*(x**2)-6x-12
+ RETURN
+END FUNCTION df
+
+END Program task4
