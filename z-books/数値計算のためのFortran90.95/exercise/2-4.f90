890812771e78b1850f1e54efce7b7942c6221abe
Taka0007 <7takacode@gmail.com>
2023-05-26 15:05:29 +0900

    solve exercise 2-4 easy

---
+++ z-books/数値計算のためのFortran90.95/exercise/2-4.f90
@@ -0,0 +1,14 @@
+! 3次元ベクトル大きさを求める
+Program Ans
+ implicit none
+ REAL vector(3)
+ REAL vecsize
+ INTEGER i
+ 
+ vector = (/ 77.7777 , 25.754987983 ,  77.2479857329 /)
+ 
+ vecsize = sqrt(vector(1)**2 + vector(2)**2 + vector(3)**2)
+
+ WRITE(*,*) vecsize
+
+END Program Ans
